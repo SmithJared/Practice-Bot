2b3dc7f2c6fe16247f221bfb4e2b1743a70bdf78
SmithJared <smithvjared@gmail.com>
2019-01-21 13:58:05 -0700

    joystick

--- PracticeBot/PracticeBot/src/main/java/frc/robot/OI.java
+++ PracticeBot/PracticeBot/src/main/java/frc/robot/OI.java
@@ -12,19 +12,31 @@ package frc.robot;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-  //// CREATING BUTTONS
+  {
+  // // CREATING BUTTONS
   // One type of button is a joystick button which is any button on a
-  //// joystick.
+  //     joystick.
   // You create one by telling it which joystick it's on and which button
   // number it is.
   // Joystick stick = new Joystick(port);
   // Button button = new JoystickButton(stick, buttonNumber);
+  drivingStick1 = new Joystick(1);
+  double value;
+  value = drivingStick1.getX();
+  value = drivingStick1.getY();
+  value = drivingStick1.getZ();
+  value = drivingStick1.getThrottle();
+  value = drivingStick1.getTwist();
+
+  boolean buttonValue;
+  buttonValue = drivingStick1.getTop();
+  buttonValue = drivingStick1.getTrigger();
 
   // There are a few additional built in buttons you can use. Additionally,
   // by subclassing Button you can create custom triggers and bind those to
   // commands the same as any other Button.
 
-  //// TRIGGERING COMMANDS WITH BUTTONS
+  // // TRIGGERING COMMANDS WITH BUTTONS
   // Once you have a button, it's trivial to bind it to a button in one of
   // three ways:
 
@@ -39,4 +51,5 @@ public class OI {
   // Start the command when the button is released and let it run the command
   // until it is finished as determined by it's isFinished method.
   // button.whenReleased(new ExampleCommand());
+}
 }
